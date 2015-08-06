613609c6912ba5cba17c1b54df17ae69befe13af
John Zhao <john@zondr.com>
2015-08-06 09:07:52 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coffeecup::Application.config.secret_key_base = '427e0871ac2b7a9ab1d1ae39adee04a0363a7b489aa3fe2a26907bada6719ce645ca0befe4aafa9a105eca75b5b1848b354a78e19443b55b491fdbfc19c2488c'
