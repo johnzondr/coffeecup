f7e2db2c46b6ee28e0318936c206efe0008dc95c
John Zhao <john@zondr.com>
2015-09-02 00:49:19 +0000

    pic sm for views

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -5,6 +5,6 @@ class Friendship < ActiveRecord::Base
 	def as_json(options = {})
 		friend = User.find(self.friend_id)
 		json_data = super(options)
-		json_data.merge(:first_name=> friend.first_name, :last_name => friend.last_name)
+		json_data.merge(:first_name=> friend.first_name, :last_name => friend.last_name, :pic_sm => friend.pic_sm)
 	end
 end
