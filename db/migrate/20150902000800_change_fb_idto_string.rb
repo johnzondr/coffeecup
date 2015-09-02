cfef6fef5e56aec2367ddf79339f59368134ace3
John Zhao <john@zondr.com>
2015-09-02 00:18:12 +0000

    changed fb id to string type

---
+++ db/migrate/20150902000800_change_fb_idto_string.rb
@@ -0,0 +1,5 @@
+class ChangeFbIdtoString < ActiveRecord::Migration
+  def change
+  	change_column :users, :fb_id, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150901195550) do
+ActiveRecord::Schema.define(version: 20150902000800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 20150901195550) do
     t.integer  "saved_area_id"
     t.float    "version"
     t.string   "device_token"
-    t.integer  "fb_id"
+    t.string   "fb_id"
     t.string   "pic_sm"
   end
 
