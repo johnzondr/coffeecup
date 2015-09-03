9b95d9764e8e6e166751cc5e6768dbe7574b7cfb
John Zhao <john@zondr.com>
2015-09-03 00:21:01 +0000

    remove self send method

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -18,34 +18,34 @@ class Checkin < ActiveRecord::Base
 		end
 	end
 
-	def self.send
-	    pusher = Grocer.pusher(
-	      certificate: Rails.root.join("lib", "prod.pem").to_s,
-	      #certificate: "#{path}/lib/cert.pem",     # required
-	      passphrase:  "zondr",                       # optional
-	      gateway:     "gateway.push.apple.com", # optional; See note below.
-	      port:        2195,                     # optional
-	      retries:     3                         # optional
-	    )
+	# def self.send
+	#     pusher = Grocer.pusher(
+	#       certificate: Rails.root.join("lib", "prod.pem").to_s,
+	#       #certificate: "#{path}/lib/cert.pem",     # required
+	#       passphrase:  "zondr",                       # optional
+	#       gateway:     "gateway.push.apple.com", # optional; See note below.
+	#       port:        2195,                     # optional
+	#       retries:     3                         # optional
+	#     )
 
-	    notification = Grocer::Notification.new(
-	      device_token:      "5582aa602520add99ce6714414a6a5ea89d700e1e71a2406aff993bc22f65a59",
-	      alert:             "James did this work?",
-	      badge:             2,
-	      category:          "a category",         # optional; used for custom notification actions
-	      sound:             "siren.aiff",         # optional
-	      expiry:            Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
-	      identifier:        1234,                 # optional; must be an integer
-	      content_available: true                  # optional; any truthy value will set 'content-available' to 1
-	    )
+	#     notification = Grocer::Notification.new(
+	#       device_token:      "5582aa602520add99ce6714414a6a5ea89d700e1e71a2406aff993bc22f65a59",
+	#       alert:             "James did this work?",
+	#       badge:             2,
+	#       category:          "a category",         # optional; used for custom notification actions
+	#       sound:             "siren.aiff",         # optional
+	#       expiry:            Time.now + 60*60,     # optional; 0 is default, meaning the message is not stored
+	#       identifier:        1234,                 # optional; must be an integer
+	#       content_available: true                  # optional; any truthy value will set 'content-available' to 1
+	#     )
 
-	    pusher.push(notification) # return value is the number of bytes sent successfully
+	#     pusher.push(notification) # return value is the number of bytes sent successfully
 
 
-	    # pusher = Grocer.pusher(connection_opions)
-	    # notifications.each do |notification|
-	    #   pusher.push(notification)t
-	    # end
-	end 
+	#     # pusher = Grocer.pusher(connection_opions)
+	#     # notifications.each do |notification|
+	#     #   pusher.push(notification)t
+	#     # end
+	# end 
 
 end
